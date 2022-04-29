7c9a8dfe64ec1bf6d8e6273137c6f01bbefb8a25
Yogesh <yogeshkr210@gmail.com>
2022-04-29 23:30:46 +0530

    Rating star of products now showing properly

--- pages/explore.js
+++ pages/explore.js
@@ -7,20 +7,20 @@ const Explore = ({ category }) => {
   const [items, setitems] = useState(category);
   return (
     <>
-      <section class="text-gray-600 body-font">
-        <div class="container px-10 py-10 mx-auto">
-          <div class="flex flex-wrap -m-4">
+      <section className="text-gray-600 body-font">
+        <div className="container px-10 py-10 mx-auto">
+          <div className="flex flex-wrap -m-4">
             {items.map((item, i) => {
               return (
                 <Link href={"category/" + item.id} passHref>
-                  <div class="xl:w-1/3 md:w-1/2 p-4">
+                  <div className="xl:w-1/3 md:w-1/2 p-4">
                       <a href=''>
-                        <div key={i} class="border border-gray-200 p-6 rounded-lg">
-                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                        <div key={i} className="border border-gray-200 p-6 rounded-lg">
+                            <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                 <Image src={item.url || 'https://uxwing.com/wp-content/themes/uxwing/download/08-computers-mobile-hardware/mobile-phone.svg'} height={25} width={25} alt="ss" />
                             </div>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-2">{item.title}</h2>
-                        <p class="leading-relaxed text-base">{item.description}</p>
+                        <h2 className="text-lg text-gray-900 font-medium title-font mb-2">{item.title}</h2>
+                        <p className="leading-relaxed text-base">{item.description}</p>
                         </div>
                       </a>
                   </div>
--- pages/product/[product_id].js
+++ pages/product/[product_id].js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 const Product = (props) => {
 const [items, setitems] = useState(props);
+console.log(items.product.rating);
+const rating=items.product.rating;
 return (
 <>
   <section className="text-gray-600 bg-gray-100 body-font overflow-hidden">
@@ -20,27 +22,27 @@ return (
           </h1>
           <div className="flex mb-4">
             <span className="flex items-center">
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+              <svg fill={rating>=1?'currentColor':'none'} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
                 strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
                 </path>
               </svg>
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+              <svg fill={rating>=2?'currentColor':'none'} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
                 strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
                 </path>
               </svg>
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+              <svg fill={rating>=3?'currentColor':'none'} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
                 strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
                 </path>
               </svg>
-              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+              <svg fill={rating>=4?'currentColor':'none'} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
                 strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
                 </path>
               </svg>
-              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+              <svg fill={rating==5?'currentColor':'none'} stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                 className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
                 </path>
@@ -49,7 +51,7 @@ return (
                 {items.product.rating} Reviews
               </span>
             </span>
-            <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+            {/* <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
               <a className="text-gray-500">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                   className="w-5 h-5" viewBox="0 0 24 24">
@@ -72,7 +74,7 @@ return (
                   </path>
                 </svg>
               </a>
-            </span>
+            </span> */}
           </div>
           <p className="leading-relaxed">{items.product.description}</p>
           <div className="flex">
