346e7e6a9fa0f719e374095e668d8737317636f3
Yogesh <yogeshkr210@gmail.com>
2022-04-29 12:16:24 +0530

    Explore page Design is updated

--- pages/explore.js
+++ pages/explore.js
@@ -7,27 +7,42 @@ const Explore = ({ category }) => {
   console.log(items);
   return (
     <>
-    <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-wrap -m-4">
-      {items.map((item, i) => {
-        return (
-          <Link  href={"category/" + item.id} passHref>
-              <div key={i} class="lg:w-1/3 sm:w-1/2 p-4">
-                        <div class="flex relative">
-                        <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://media.istockphoto.com/photos/healthy-rainbow-colored-fruits-and-vegetables-smoothies-isolated-on-picture-id1304936721?b=1&k=20&m=1304936721&s=170667a&w=0&h=DiGoVD2R75gCK-jJayc89NFxd3HHiKLba46Bmr5Ud_Y="/>
-                        <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                            <h2 class="tracking-widest text-lg title-font font-medium text-indigo-500 mb-1">{item.title}</h2>
-                            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                        </div>
-                        </div>
+      <section class="text-gray-600 body-font">
+        <div class="container px-10 py-10 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            {items.map((item, i) => {
+              return (
+                <Link href={"category/" + item.id} passHref>
+                  <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div key={i} class="border border-gray-200 p-6 rounded-lg">
+                      <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          class="w-6 h-6"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                        </svg>
+                      </div>
+                      <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                      {item.title}
+                      </h2>
+                      <p class="leading-relaxed text-base">
+                        Fingerstache flexitarian street art 8-bit waist co,
+                        subway tile poke farm.
+                      </p>
                     </div>
-          </Link>
-        );
-        })}
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
-        </div>
-    </section>
+      </section>
     </>
   );
 };
